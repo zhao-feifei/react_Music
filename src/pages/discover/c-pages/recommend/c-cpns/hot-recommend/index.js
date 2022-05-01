b911092254a32050df9d98ce52e2d68c29de59de
zhao-feifei <398983914@qq.com>
2022-05-01 13:37:05 +0800

    热门推荐数据请求完成

--- src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
+++ src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -1,14 +1,33 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect } from 'react';
+import { dispatch, useDispatch, shallowEqual, useSelector } from 'react-redux';
 import { HotRecommendWrapper } from './style';
 import HYThemeHeaderRCM from '@/components/theme-header-rcm';
+import { getHotRecommendAction } from '../../store/actionCreators';
 
 const HYHotRecommend = memo(() => {
+  const { hotRecommends } = useSelector(
+    (state) => ({
+      hotRecommends: state.getIn(['recommend', 'hotRecommends']),
+    }),
+    shallowEqual
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getHotRecommendAction(8));
+  });
   return (
     <HotRecommendWrapper>
       <HYThemeHeaderRCM
         title="热门推荐"
         keywords={['华语', '流行', '民谣', '摇滚', '电子']}
       ></HYThemeHeaderRCM>
+      <div className="recommend-list">
+        {hotRecommends.map((item, index) => {
+          return <div>{item.name}</div>;
+        })}
+      </div>
     </HotRecommendWrapper>
   );
 });
--- src/pages/discover/c-pages/recommend/store/actionCreators.js
+++ src/pages/discover/c-pages/recommend/store/actionCreators.js
@@ -1,15 +1,28 @@
 import * as actionTypes from './constants';
 
-import { getTopBanners } from '@/services/recommend';
+import { getTopBanners, getHotRecommends } from '@/services/recommend';
 const changeTopBannersAction = (res) => ({
   type: actionTypes.CHANGE_TOP_BANNERS,
   topBanners: res.banners,
+});
+
+const changeHotRecommendAction = (res) => ({
+  type: actionTypes.CHANGE_HOT_RECOMMEND,
+  hotRecommends: res.result,
 });
 
 export const getTopBannersAction = () => {
   return (dispatch) => {
     getTopBanners().then((res) => {
       dispatch(changeTopBannersAction(res));
+    });
+  };
+};
+
+export const getHotRecommendAction = (limit) => {
+  return (dispatch) => {
+    getHotRecommends(limit).then((res) => {
+      dispatch(changeHotRecommendAction(res));
     });
   };
 };
